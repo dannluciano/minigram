f5bef270d696b62da774f5b7298080d2f638d463
Dann Luciano <dannluciano@gmail.com>
2018-02-23 13:29:47 -0300

    Closed #1, #2

--- public/js/aplicacao.js
+++ public/js/aplicacao.js
@@ -1,33 +1,33 @@
-const filters = ["", "1977", "aden", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",  "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua", "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"]
+const filters = ['', '1977', 'aden', 'brannan', 'brooklyn', 'clarendon', 'earlybird', 'gingham', 'hudson', 'inkwell', 'kelvin', 'lark', 'lofi', 'maven', 'mayfair', 'moon', 'nashville', 'perpetua', 'reyes', 'rise', 'slumber', 'stinson', 'toaster', 'valencia', 'walden', 'willow', 'xpro2']
 window.URL = window.URL || window.webkitURL
 
-var fileElem = document.getElementById("foto"),
-  img = document.getElementById("image-render")
+const fileElem = document.getElementById('fileButton')
+const img = document.getElementById('image-render')
 
-function handleFiles(files) {
+function handleFiles (files) {
   if (!files.length) {
-    console.log("No files selected!")
+    console.log('No files selected!')
   } else {
     const urlImage = window.URL.createObjectURL(files[0])
-    img.src = urlImage;
+    img.src = urlImage
     fileElem.classList.toggle('d-none')
     img.classList.toggle('d-none')
 
-    for(let filter of filters) {
-      const label = document.createElement("label")
-      label.style.display = "inline"
+    for (let filter of filters) {
+      const label = document.createElement('label')
+      label.style.display = 'inline'
       label.onclick = applyFilter
-      
-      const inputFilter = document.createElement("input")
-      inputFilter.type = "radio"
-      inputFilter.name = "filtro"
+
+      const inputFilter = document.createElement('input')
+      inputFilter.type = 'radio'
+      inputFilter.name = 'filtro'
       inputFilter.value = filter
       label.appendChild(inputFilter)
 
-      const imgFilter = document.createElement("img")
+      const imgFilter = document.createElement('img')
       imgFilter.src = urlImage
-      imgFilter.className = filter + " rounded"
-      imgFilter.style.width = "8em"
+      imgFilter.className = filter + ' rounded'
+      imgFilter.style.width = '8em'
       label.appendChild(imgFilter)
 
       filtersContainer.appendChild(label)
@@ -37,5 +37,12 @@ function handleFiles(files) {
 
 function applyFilter () {
   const selectedRadio = document.querySelector('input:checked')
-  img.className = "img-fluid rounded mx-auto " + selectedRadio.value
+  if (selectedRadio) {
+    img.className = 'img-fluid rounded mx-auto ' + selectedRadio.value
+  }
 }
+
+filtersContainer.addEventListener('wheel', function (e) {
+  this.scrollLeft += (e.deltaY * 50)
+  e.preventDefault()
+})
